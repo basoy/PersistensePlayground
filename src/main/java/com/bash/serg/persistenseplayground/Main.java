9b48bbcfaae7baed41a094942a2215d417480fb2
Serg Bash <basoy1988@mail.ru>
2015-12-16 12:13:23 +0200

    Refactor project. Change from hibernate->jpa

--- src/main/java/com/bash/serg/persistenseplayground/Main.java
+++ src/main/java/com/bash/serg/persistenseplayground/Main.java
@@ -1,15 +1,11 @@
 package com.bash.serg.persistenseplayground;
 
 import com.bash.serg.persistenseplayground.entity.Inventory;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.LinkedList;
 
 /**
  * Created by Serg Bash on 12/14/2015.
@@ -18,17 +14,15 @@ public class Main {
 
     public static void main(String[] args)
     {
-
-
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit_demo");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
 
         transaction.begin();
 
-        Inventory car1 = new Inventory("Maybach",2013,"silver");
-        Inventory car2 = new Inventory("Ford",2014,"black");
-        Inventory car3 = new Inventory("Lamborgini",2015,"grey");
+        Inventory car1 = new Inventory("Maybach", 2013, "silver");
+        Inventory car2 = new Inventory("Ford", 2014, "black");
+        Inventory car3 = new Inventory("Lamborgini", 2015, "grey");
 
         try
         {
@@ -36,7 +30,6 @@ public class Main {
             entityManager.persist(car2);
             entityManager.persist(car3);
             transaction.commit();
-
         }
         catch (Exception ex)
         {
@@ -47,7 +40,5 @@ public class Main {
         {
             entityManager.close();
         }
-
     }
-
 }
