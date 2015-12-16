2efea135b5aa77283280d64c068a3ae8e4d60783
Serg Bash <basoy1988@mail.ru>
2015-12-16 20:11:14 +0200

    Add console manipulations

--- src/main/java/com/bash/serg/persistenseplayground/Main.java
+++ src/main/java/com/bash/serg/persistenseplayground/Main.java
@@ -6,11 +6,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Serg Bash on 12/14/2015.
  */
 public class Main {
+    private final static String INSERT = "insert";
+    private final static String SELECT = "select";
+    private final static String EXIT = "exit";
 
     public static void main(String[] args)
     {
@@ -20,18 +28,35 @@ public class Main {
 
         transaction.begin();
 
-        Inventory car1 = new Inventory("Maybach", 2013, "silver");
-        Inventory car2 = new Inventory("Ford", 2014, "black");
-        Inventory car3 = new Inventory("Lamborgini", 2015, "grey");
-
+        Scanner keyboard = new Scanner(System.in);
+        Inventory car = new Inventory();
         try
         {
-            entityManager.persist(car1);
-            entityManager.persist(car2);
-            entityManager.persist(car3);
+            while(keyboard.hasNext()) {
+                String s1 = keyboard.nextLine();
+                String [] res = new String[3];
+                if(s1.contains(INSERT)){
+                    for(int i = 0; i < 2; i++) {
+                        res = s1.split(",");
+                    }
+                    List <String> cars = Arrays.asList(res);
+                    car = new Inventory(cars.get(0).replace("insert ", ""),
+                            Integer.parseInt(cars.get(1)), cars.get(2));
+                    break;
+
+                }
+                if(s1.equals(SELECT)){
+                    System.out.println("select");
+                }
+                if(s1.equals(EXIT)) {
+                    break;
+                }
+            }
+            entityManager.persist(car);
             transaction.commit();
-        }
-        catch (Exception ex)
+            System.exit(0);
+
+        } catch (Exception ex)
         {
             System.out.println(ex.getMessage());
             transaction.rollback();
