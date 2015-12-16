e67770e9952683d3032d2420ed187d003707622b
Serg Bash <basoy1988@mail.ru>
2015-12-16 12:10:03 +0200

    Refactor project. Change from hibernate->jpa

--- src/main/java/com/bash/serg/persistenseplayground/Main.java
+++ src/main/java/com/bash/serg/persistenseplayground/Main.java
@@ -5,25 +5,49 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.LinkedList;
+
 /**
  * Created by Serg Bash on 12/14/2015.
  */
 public class Main {
 
-public static void main(String [] args){
-    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    Session session = sessionFactory.openSession();
-    session.beginTransaction();
+    public static void main(String[] args)
+    {
 
-    Inventory car1 = new Inventory("Maybah", 2012, "silver");
-    Inventory car2 = new Inventory("Ford", 2015, "black");
-    Inventory car3 = new Inventory("Lamborgini", 2015, "grey");
 
-    session.save(car1);
-    session.save(car2);
-    session.save(car3);
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit_demo");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
 
-    session.getTransaction().commit();
-    session.close();
-}
+        transaction.begin();
+
+        Inventory car1 = new Inventory("Maybach",2013,"silver");
+        Inventory car2 = new Inventory("Ford",2014,"black");
+        Inventory car3 = new Inventory("Lamborgini",2015,"grey");
+
+        try
+        {
+            entityManager.persist(car1);
+            entityManager.persist(car2);
+            entityManager.persist(car3);
+            transaction.commit();
+
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+            transaction.rollback();
+        }
+        finally
+        {
+            entityManager.close();
+        }
+
+    }
+
 }
